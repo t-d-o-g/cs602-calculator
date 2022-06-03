fb5ebb04674dedd8108352dac8dca63757fe86c3
t-d-o-g <trc229@gmail.com>
2022-06-02 21:23:20 -0400

    Add exception handling for non-integer inputs, no input, and division by 0

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import javax.swing.ButtonGroup;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JRadioButton;
+import javax.swing.JOptionPane;
 
 import javax.swing.WindowConstants;
 import javax.swing.SwingUtilities;
@@ -47,6 +48,9 @@ public class Main extends javax.swing.JFrame {
         jLabelEquals = new JLabel("=");
         jLabelEquals.setBounds(265, 100, 35, 30);
         getContentPane().add(jLabelEquals);
+        jTextFieldResult = new JTextField();
+        jTextFieldResult.setBounds(295, 100, 160, 40);
+        getContentPane().add(jTextFieldResult);
         try {
             setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
             getContentPane().setLayout(null);
@@ -113,16 +117,10 @@ public class Main extends javax.swing.JFrame {
                 jTextFieldB.setBounds(170, 100, 80, 40);
                 getContentPane().add(jTextFieldB);
             }
-            {
-                jTextFieldResult = new JTextField();
-                jTextFieldResult.setBounds(295, 100, 160, 40);
-                getContentPane().add(jTextFieldResult);
-            }
             pack();
             setSize(475, 300);
         } catch (Exception e) {
             //add your error handling code here
-            e.printStackTrace();
         }
     }
 
@@ -135,21 +133,69 @@ public class Main extends javax.swing.JFrame {
 
     private void jRadioButtonAddActionPerformed(ActionEvent evt) {
         System.out.println("jRadioButtonAdd.actionPerformed, event="+evt);
-        //TODO add your code for jRadioButtonAdd.actionPerformed
+        if (jTextFieldA.getText().isEmpty() || jTextFieldB.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Error: No input in one or both text fields.");
+            return;
+        }
+        try {
+            String a = jTextFieldA.getText();
+            String b = jTextFieldB.getText();
+            int result = Integer.parseInt(a) + Integer.parseInt(b);
+            jTextFieldResult.setText(String.valueOf(result));
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Error: Non integer inputs on one or both text fields.");
+        }
     }
 
     private void jRadioButtonSubtractActionPerformed(ActionEvent evt) {
         System.out.println("jRadioButtonSubtract.actionPerformed, event="+evt);
-        //TODO add your code for jRadioButtonSubtract.actionPerformed
-    }
+        if (jTextFieldA.getText().isEmpty() || jTextFieldB.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Error: No input in one or both text fields.");
+            return;
+        }
+        try {
+            String a = jTextFieldA.getText();
+            String b = jTextFieldB.getText();
+            int result = Integer.parseInt(a) - Integer.parseInt(b);
+            jTextFieldResult.setText(String.valueOf(result));
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Error: Non integer inputs on one or both text fields.");
+        }
+}
 
     private void jRadioButtonMultiplyActionPerformed(ActionEvent evt) {
         System.out.println("jRadioButtonMultiply.actionPerformed, event="+evt);
-        //TODO add your code for jRadioButtonMultiply.actionPerformed
-    }
+        if (jTextFieldA.getText().isEmpty() || jTextFieldB.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Error: No input in one or both text fields.");
+            return;
+        }
+        try {
+            String a = jTextFieldA.getText();
+            String b = jTextFieldB.getText();
+            int result = Integer.parseInt(a) * Integer.parseInt(b);
+            jTextFieldResult.setText(String.valueOf(result));
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Error: Non integer inputs on one or both text fields.");
+        }
+}
 
     private void jRadioButtonDivideActionPerformed(ActionEvent evt) {
         System.out.println("jRadioButtonDivide.actionPerformed, event="+evt);
-        //TODO add your code for jRadioButtonDivide.actionPerformed
+        if (jTextFieldA.getText().isEmpty() || jTextFieldB.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Error: No input in one or both text fields.");
+            return;
+        }
+        try {
+            String a = jTextFieldA.getText();
+            String b = jTextFieldB.getText();
+            if (!b.equals("0")) {
+                float floatResult = (float) Integer.parseInt(a) / Integer.parseInt(b);
+                jTextFieldResult.setText(String.valueOf(floatResult));
+            } else {
+                JOptionPane.showMessageDialog(null, "Division by zero.");
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Error: Non integer inputs on one or both text fields.");
+        }
     }
 }
